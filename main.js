b82f5c096239083e0827fd0799a6d8599963511c
takezakisibuki <kaizyudebugon@gmail.com>
2024-01-03 23:01:27 +0900

    chenge

--- main.js
+++ main.js
@@ -154,52 +154,88 @@ function subscribeToNewTodos() {
 var ls = [];
 const val = fetchCountTodos();
 val.then(result => {
-    console.log(result)
-    ls=result; // 配列の値が表示される
-    console.log(ls)
+    var pieData = [
+        {
+            value: result[0],            // 値
+            color:"#F7464A",       // 色
+            highlight: "#FF5A5E",  // マウスが載った際の色
+            label: "りんご"        // ラベル
+        },
+        {
+            value: result[1],
+            color: "#41C44E",
+            highlight: "#6CD173",
+            label: "メロン"
+        },
+        {
+            value: result[2],
+            color: "#FDB45C",
+            highlight: "#FFC870",
+            label: "みかん"
+        },
+        {
+            value: result[3],
+            color: "#AA49B8",
+            highlight: "#C583CF",
+            label: "ぶどう"
+        },
+        {
+            value: result[4],
+            color: "#4D5360",
+            highlight: "#616774",
+            label: "その他"
+        }
+        
+        ];
+        
+        window.onload = function () {
+        var ctx = document.getElementById("graph-area").getContext("2d");
+        window.myPie = new Chart(ctx).Pie(pieData);
+    };
+    
 }).catch(error => {
     console.error('Error:', error);
 });
 
 
-var pieData = [
-{
-    value: 100,            // 値
-    color:"#F7464A",       // 色
-    highlight: "#FF5A5E",  // マウスが載った際の色
-    label: "りんご"        // ラベル
-},
-{
-    value: 100,
-    color: "#41C44E",
-    highlight: "#6CD173",
-    label: "メロン"
-},
-{
-    value: 100,
-    color: "#FDB45C",
-    highlight: "#FFC870",
-    label: "みかん"
-},
-{
-    value: 100,
-    color: "#AA49B8",
-    highlight: "#C583CF",
-    label: "ぶどう"
-},
-{
-    value: 30,
-    color: "#4D5360",
-    highlight: "#616774",
-    label: "その他"
-}
+// var pieData = [
+// {
+//     value: 100,            // 値
+//     color:"#F7464A",       // 色
+//     highlight: "#FF5A5E",  // マウスが載った際の色
+//     label: "りんご"        // ラベル
+// },
+// {
+//     value: 100,
+//     color: "#41C44E",
+//     highlight: "#6CD173",
+//     label: "メロン"
+// },
+// {
+//     value: 100,
+//     color: "#FDB45C",
+//     highlight: "#FFC870",
+//     label: "みかん"
+// },
+// {
+//     value: 100,
+//     color: "#AA49B8",
+//     highlight: "#C583CF",
+//     label: "ぶどう"
+// },
+// {
+//     value: 30,
+//     color: "#4D5360",
+//     highlight: "#616774",
+//     label: "その他"
+// }
 
-];
+// ];
 
-window.onload = function () {
-var ctx = document.getElementById("graph-area").getContext("2d");
-window.myPie = new Chart(ctx).Pie(pieData);
-}
+// window.onload = function () {
+// var ctx = document.getElementById("graph-area").getContext("2d");
+// window.myPie = new Chart(ctx).Pie(pieData);
+// }
 
 
 alert(toString.call(ls));
